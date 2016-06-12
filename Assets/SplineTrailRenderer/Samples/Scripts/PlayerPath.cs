3a3d117208e7ac6eba6b979cf4330fa612649aeb
Liu Pengcheng <sirlpc@gmail.com>
2016-06-12 08:16:09 +0800

    移动统一管理

--- Assets/SplineTrailRenderer/Samples/Scripts/PlayerPath.cs
+++ Assets/SplineTrailRenderer/Samples/Scripts/PlayerPath.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class PlayerPath : MonoBehaviour 
 {
 	public SplineTrailRenderer trailReference;
-	public string groundLayerName = "Water";
-	public string playerLayerName = "Default";
+	public const string GroundLayerName = "Water";
+	public const string PlayerLayerName = "Default";
 	public Vector3 trailOffset = new Vector3(0, 0.02f, 0);
 
 	private bool playerSelected = false;
 	
-	void Update () 
+	void Update_kill () 
 	{
 		if(Input.GetMouseButtonDown(0))
 		{
@@ -21,13 +21,13 @@ public class PlayerPath : MonoBehaviour
             if (Physics.Raycast(ray, out hit, float.MaxValue))
             {
                 var ln = LayerMask.LayerToName(hit.transform.gameObject.layer);
-                if (!ln.Equals(playerLayerName) || hit.transform.root != this.transform)
+                if (!ln.Equals(PlayerLayerName) || hit.transform.root != this.transform)
                     return;
 
-				playerSelected = true;
-				MoveOnFloor();
-				trailReference.Clear();
-			}
+                playerSelected = true;
+                MoveOnFloor();
+                trailReference.Clear();
+            }
 		}
 		else if(Input.GetMouseButtonUp(0))
 		{
@@ -40,11 +40,30 @@ public class PlayerPath : MonoBehaviour
 		}
 	}
 
+    public void StartMove()
+    {
+        playerSelected = true;
+        MoveOnFloor();
+        trailReference.Clear();
+    }
+
+    public void InMove()
+    {
+        if (!playerSelected)
+            return;
+        MoveOnFloor();
+    }
+
+    public void EndMove()
+    {
+        playerSelected = false;
+    }
+
 	void MoveOnFloor()
 	{
 		RaycastHit hit;
 		if(Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, 
-			Input.mousePosition.y, 0)), out hit, float.MaxValue, LayerNameToIntMask(groundLayerName)))
+			Input.mousePosition.y, 0)), out hit, float.MaxValue, LayerNameToIntMask(GroundLayerName)))
 		{
 			trailReference.transform.position = hit.point + trailOffset;
 		}
